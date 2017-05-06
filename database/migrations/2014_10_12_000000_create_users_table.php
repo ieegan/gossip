da7d3176d89e7ecdf85fc90035a46f986a09cc55
Ibrahim Eegan <eegan@thepaperidea.com>
2017-05-06 23:49:50 +0500

    Length fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('email', 250);
             $table->string('email')->unique()->nullable();
             $table->string('password')->nullable();
             $table->rememberToken();
